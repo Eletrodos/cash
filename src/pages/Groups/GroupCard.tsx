0adf674b6e48d01b1dcdca23c4f6ed0660c9c718
Matheus Moura <matheus.moura13@gmail.com>
2019-07-20 16:46:11 -0300

    Cria compoente GroupCard

--- src/pages/Groups/GroupCard.tsx
+++ src/pages/Groups/GroupCard.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import PeopleIcon from "@material-ui/icons/People";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import {
   Divider,
   Card,
   Typography,
   CardContent,
-  makeStyles
+  makeStyles,
+  Box,
+  Chip,
+  Tooltip
 } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   card: {
     width: "100%"
+  },
+  chip: {
+    marginTop: 10,
+    marginRight: 4
   }
 }));
 
@@ -18,20 +27,50 @@ interface IGroupCardProps {
   name: string;
   members: number;
   userCoins: number;
+  isAdmin?: boolean;
 }
 
 /** Exibe as informações de um determinado grupo */
-const GroupCard: React.FC<IGroupCardProps> = ({ name, members, userCoins }) => {
+const GroupCard: React.FC<IGroupCardProps> = ({
+  isAdmin,
+  name,
+  members,
+  userCoins
+}) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography>{name}</Typography>
+        <Typography variant="subtitle2">{name.toUpperCase()}</Typography>
         <Divider variant="fullWidth" />
-        <div>
-          Membros: {members} <PeopleIcon />
-        </div>
-        <div>Seu saldo: {userCoins}¢</div>
+        <Box>
+          <Tooltip title="Quantidade membros que frequentam este grupo">
+            <Chip
+              className={classes.chip}
+              size="small"
+              icon={<PeopleIcon />}
+              label={members}
+            />
+          </Tooltip>
+          <Tooltip title={"Quantidade de moedas que você possui neste grupo"}>
+            <Chip
+              className={classes.chip}
+              size="small"
+              icon={<AttachMoneyIcon />}
+              label={userCoins}
+            />
+          </Tooltip>
+          {isAdmin && (
+            <Tooltip title={"Você é administrador deste grupo"}>
+              <Chip
+                className={classes.chip}
+                size="small"
+                icon={<VerifiedUserIcon />}
+                label="admin"
+              />
+            </Tooltip>
+          )}
+        </Box>
       </CardContent>
     </Card>
   );
